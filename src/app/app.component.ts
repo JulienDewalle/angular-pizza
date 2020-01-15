903862724d2ee505e7313e06814f645aa18d55fe
Dewalle Julien <julien.dewalle@hotmail.fr>
2020-01-15 15:59:14 +0100

    display pizza list and selected pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+
+class Pizza {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const PIZZAS : Pizza[] = [
+  { id: 1, name: 'Reine', price: 12 },
+  { id: 2, name: '4 fromages', price: 13 },
+  { id: 3, name: 'Orientale', price: 11 },
+  { id: 4, name: 'Cannibale', price: 9 }
+];
 
 class Pizza {
   id: number;
@@ -13,9 +27,19 @@ class Pizza {
 })
 export class AppComponent {
   title = 'pizzaparty';
+<<<<<<< Updated upstream
   pizza: Pizza = {
     id : 1,
     name: 'Reine',
     price: 12
   };
+=======
+  selectedPizza: Pizza;
+  pizzas: Pizza[] = PIZZAS;
+onSelect(pizza: Pizza): void{
+  //on recup au click la pizza
+  console.log(pizza);
+  this.selectedPizza = pizza;
+ }
+>>>>>>> Stashed changes
 }
